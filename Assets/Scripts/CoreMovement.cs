8da010ce896b5185e8510a33279ae3beb51c7f41
Vinit Rao <154247095+vinit-rao@users.noreply.github.com>
2026-04-22 13:00:31 -0400

    rough jumping mechanic & tag system to check jumping

--- Assets/Scripts/CoreMovement.cs
+++ Assets/Scripts/CoreMovement.cs
@@ -3,36 +3,69 @@ using UnityEngine;
 public class CoreMovement : MonoBehaviour
 {
     [Header("Movement Stats")]
-    [SerializeField] private float maxSpeed = 8f;
-    [SerializeField] private float accelForce = 40f;
-    [SerializeField] private float decelForce = 60f;
-
+    [SerializeField] private float maxSpeed = 20f;
+    [SerializeField] private float accelForce = 30f;
+    [SerializeField] private float decelForce = 50f;
+    [SerializeField] private float jumpForce = 10f;
     private Rigidbody2D rb;
-    private float moveInput;
+    private Collider2D collision;
+    private float hInput;
+    private float vInput;
+
+    private bool canJump;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        collision = GetComponent<Collider2D>();
     }
 
     void Update()
     {
         // will output 1, 0, -1
-        moveInput = Input.GetAxisRaw("Horizontal");
+        hInput = Input.GetAxisRaw("Horizontal");
+
+        vInput = Input.GetAxisRaw("Vertical");
     }
 
     void FixedUpdate()
     {
-        // 1. How fast do we WANT to be going right now?
-        float targetSpeed = moveInput * maxSpeed;
+        // direction * speed value
+        float targetSpeed = hInput * maxSpeed;
 
-        // 2. Are we pressing a key (accelerating) or letting go (decelerating)?
-        float currentAccel = (Mathf.Abs(moveInput) > 0.01f) ? acceleration : deceleration;
+        // if key pressed or not it decides whether to accelerate or decelerate
+        float accelCurrent = (Mathf.Abs(hInput) > 0.01f) ? accelForce : decelForce;
 
-        // 3. Calculate the smooth transition from our current speed to our target speed
-        float newSpeed = Mathf.MoveTowards(rb.linearVelocity.x, targetSpeed, currentAccel * Time.fixedDeltaTime);
+        // using time to transition the current player speed with the target speed
+        float newSpeed = Mathf.MoveTowards(rb.linearVelocity.x, targetSpeed, accelCurrent * Time.fixedDeltaTime);
 
-        // 4. Apply the new speed to the Rigidbody, leaving gravity (Y) exactly as it is
+        // only applying to x value of rb
         rb.linearVelocity = new Vector2(newSpeed, rb.linearVelocity.y);
+
+        // Debug.Log(accelCurrent);
+        // Debug.Log(newSpeed);
+        // Debug.Log(canJump);
+
+        if (vInput > 0f && canJump == true)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            canJump = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            canJump = false;
+        }
     }
 }
