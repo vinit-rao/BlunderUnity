37d8567af994a9de0b3b553f8b0513fe5a30e40b
Zaid Ahmad <109442753+zaidahmad16@users.noreply.github.com>
2026-04-22 22:07:02 -0400

    updated  coremovements.cs code
    updated code

--- Assets/Scripts/CoreMovement.cs
+++ Assets/Scripts/CoreMovement.cs
@@ -1,71 +1,37 @@
 using UnityEngine;
 
-public class CoreMovement : MonoBehaviour
+public class coreMovement : MonoBehaviour
 {
-    [Header("Movement Stats")]
-    [SerializeField] private float maxSpeed = 20f;
-    [SerializeField] private float accelForce = 30f;
-    [SerializeField] private float decelForce = 50f;
-    [SerializeField] private float jumpForce = 10f;
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpForce = 7f;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private Transform groundCheck;
+
     private Rigidbody2D rb;
-    private Collider2D collision;
-    private float hInput;
-    private float vInput;
+    //private Animator animator;
+    private bool isGrounded;
 
-    private bool canJump;
-
-    void Awake()
+    void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        collision = GetComponent<Collider2D>();
+        //animator = GetComponent<Animator>();
     }
 
     void Update()
     {
-        // will output 1, 0, -1
-        hInput = Input.GetAxisRaw("Horizontal");
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
 
-        vInput = Input.GetAxisRaw("Vertical");
-    }
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        rb.linearVelocity = new Vector2(horizontal * moveSpeed, rb.linearVelocity.y);
 
-    void FixedUpdate()
-    {
-        // direction * speed value
-        float targetSpeed = hInput * maxSpeed;
+        if (Input.GetButtonDown("Jump") && isGrounded)
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
 
-        // if key pressed or not it decides whether to accelerate or decelerate
-        float accelCurrent = (Mathf.Abs(hInput) > 0.01f) ? accelForce : decelForce;
+        if (horizontal != 0)
+            transform.localScale = new Vector3(Mathf.Sign(horizontal), 1f, 1f);
 
-        // using time to transition the current player speed with the target speed
-        float newSpeed = Mathf.MoveTowards(rb.linearVelocity.x, targetSpeed, accelCurrent * Time.fixedDeltaTime);
-
-        // only applying to x value of rb
-        rb.linearVelocity = new Vector2(newSpeed, rb.linearVelocity.y);
-
-        // Debug.Log(accelCurrent);
-        // Debug.Log(newSpeed);
-        // Debug.Log(canJump);
-
-        if (vInput > 0f && canJump == true)
-        {
-            rb.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
-        }
-    }
-
-
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            canJump = true;
-        }
-    }
-
-    void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            canJump = false;
-        }
+        //animator.SetFloat("Speed", Mathf.Abs(horizontal));
+        //animator.SetBool("IsGrounded", isGrounded);
+        //animator.SetFloat("VerticalVelocity", rb.linearVelocity.y);
     }
 }
